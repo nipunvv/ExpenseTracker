d305e15bfe9fe60d68d5cf0d64a6641fa03207f4
Nipun VV <nipunvv@gmail.com>
2021-01-19 00:43:55 +0530

    Show categories in home page

--- lib/main.dart
+++ lib/main.dart
@@ -43,18 +43,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Map<String, Object>> _txTypes = [
+    {'icon': Icons.shopping_cart, 'title': 'Groceries'},
+    {'icon': Icons.train, 'title': 'Travel'},
+    {'icon': Icons.shopping_basket, 'title': 'Beauty'},
+    {'icon': Icons.theaters, 'title': 'Entertainment'},
+    {'icon': Icons.whatshot, 'title': 'Other'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Expense Tracker',
+            Container(
+              height: 500,
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(horizontal: 5, vertical: 7),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Icon(
+                            _txTypes[index]['icon'],
+                          ),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      _txTypes[index]['title'],
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      'Monthly total comes here',
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
