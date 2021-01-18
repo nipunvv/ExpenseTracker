e5160a38b4120bec4a9d2c65be7998b0b1e933f8
Nipun VV <nipunvv@gmail.com>
2021-01-19 00:47:07 +0530

    Refactor app name

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Expenses',
+      title: 'ExpenseTracker',
       theme: ThemeData(
         primarySwatch: Colors.purple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
